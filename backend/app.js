679bff20712b937023d357e7f8e5688144126588
Marielle Domenges <marielle.domenges@gmail.com>
2023-12-21 18:58:17 +0100

    Piiquante

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+// interactions express avec la base de données MongoDB
+const mongoose = require('mongoose');
+const path = require('path');
+
+// déclaration des middlewares routes
+const stuffRoutes = require('./routes/stuffs');
+const userRoutes = require('./routes/user');
+
+// connection app avec base donnée
+mongoose.connect('mongodb+srv://Scorsese:Holbein1533@clusterpiiquante.44b649p.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
+app.use(express.json());
+
+// CORS
 app.use((req, res, next) => {
-    console.log('Requête reçue');
+    res.setHeader('Access-Control-Allow-Origin', 'http://:localhost:4200');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
+  app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.json({ message: 'votre requête a bien été reçue !'});
-    next();
-});
+  // enregistrement des middlewares routes dans une route unique
+  app.use('/api/stuff', stuffRoutes);
+  app.use('/api/auth', userRoutes);
+  app.use('/images', express.static(path.join(__dirname, 'images')))
 
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-});
+  module.exports = app;
 
-module.exports = app;
---
+++ backend/models/thing.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const clientSchema = mongoose.Schema({
+    emailClient : {type : String, required : true},
+    passwordClient : {type : String, required : true},
+});
+module.exports = mongoose.model('Thing', clientSchema);
+
+const sauceSchema = mongoose.Schema({
+    // identifiant MongoDB unique de l'utilisateur qui a créé la sauce
+    userId : {type:String, required : true},
+    // nom de la sauce
+    name : {type:String, required : true},
+    // fabricant de la sauce
+    manufacturer : {type:String, required : true},
+    // description de la sauce
+    description : {type:String, required : true},
+    // le principal ingrédient épicé de la sauce
+    mainPepper :  {type:String, required : true},
+    // l'URL de l'image de la sauce téléchargée par l'utilisateur
+    imageUrl :  {type:String, required : true},
+    // nombre entre 1 et 10 décrivant la sauce
+    heat : {type : Number, required : true},
+    // nombre d'utilisateurs qui aiment (= likent) la sauce
+    likes : {type : Number, required : true},
+    // nombre d'utilisateurs qui n'aiment pas (= dislike) la sauce
+    dislikes : {type : Number, required : true},
+    // tableau des identifiants des utilisateurs 
+    // qui ont aimé (= liked) la sauce 
+    usersLiked : [ "String <userId>" ],
+    // tableau des identifiants des utilisateurs 
+    // qui n'ont pas aimé (= disliked) la sauce
+    usersDisliked : [ "String <userId>" ],
+})
+
+module.exports = mongoose.model('Thing', sauceSchema);
